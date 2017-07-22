e073429a9984029870d3e34b5fc397356a569540
1697115721 <1697115721@qq.com>
2017-07-22 15:56:40 +0800

    创建项目

---
+++ src/org/lanqiao/pojo/UserInfo.java
@@ -0,0 +1,18 @@
+package org.lanqiao.pojo;
+
+/**    
+ * @author 刘双平
+ * @date 2017年7月22日 下午3:43:55 
+ * @Description: TODO(用一句话描述该文件做什么)   
+ */
+public class UserInfo {
+    private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
